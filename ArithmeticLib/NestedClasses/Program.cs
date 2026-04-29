1142e64a199f48d1738df1544eb8a5f0bf50a37e
Nandeesh Hiremath <nandeesh.hiremath@acuvate.com>
2026-04-29 15:59:22 +0530

    other updated files

---
+++ ArithmeticLib/NestedClasses/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClasses
+{
+
+    class OuterClass
+    {
+        public class InnerClass
+        {
+            public void Message()
+            {
+                Console.WriteLine("message from inner class");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OuterClass.InnerClass inClass = new OuterClass.InnerClass();
+            inClass.Message();
+
+        }
+    }
+}
